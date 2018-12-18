b8317c2361fce414c88b73661cfb0f48710459a1
Priyansh <mr.priyanshgaharana@gmail.com>
2018-12-18 19:50:08 +0530

    18 Dec Icpc

---
+++ temp.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+#include<boost/multiprecision/cpp_int.hpp>
+namespace mp=boost::multiprecision;
+using namespace mp;*/
+//cpp_int
+
+#define mem(A) memset(A,0,sizeof(A))
+#define loop(i,a,n) for(long long i=a;i<n;i++)
+#define pb push_back
+#define mkp make_pair
+#define prq priority_queue
+#define fir first
+#define sec second
+#define all(A) A.begin(),A.end()
+#define mod 1000000007
+#define take(t) ll t;cin>>t;while(t--)
+#define en "\n"
+
+typedef long long ll;
+typedef pair<ll,ll> pll;
+
+int main()
+{   //freopen("input.txt", "r", stdin);
+    //freopen(output.txt","w",stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+}
+
